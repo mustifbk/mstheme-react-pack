5de8ad528dd33cfdcc4a64cdcd1277f8c3b099ed
mustifibk <musti_591@hotmail.com>
2022-09-02 22:08:56 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Logo from './logo.svg'
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" alt="" srcset="" />
+      <img src={Logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
